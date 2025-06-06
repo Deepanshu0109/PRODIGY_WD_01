661602be095faaa3d2a0af4d758f662d507f9e23
Deepanshu <deepanshurai890@gmail.com>
2025-06-06 12:58:48 +0530

    completed home

---
+++ script.js
@@ -0,0 +1,32 @@
+let displayBottle = document.getElementById("p1");
+let bottles = document.querySelectorAll(".products");
+let bottleBG = document.getElementById("displayBG2");
+let belowBG = document.getElementById("displayBG");
+
+
+bottles.forEach(products => {
+    products.addEventListener("click", function () {
+        displayBottle.src = products.src;
+        displayBottle.classList.add("bottleAnimate");
+        displayBottle.addEventListener("animationend", () => {
+            displayBottle.classList.remove("bottleAnimate");
+        }, { once: true });
+
+        const path = products.src;
+        let color = "#50C878";
+        if (path.includes("yellow")) color = "gold";
+        else if (path.includes("red")) color = "palevioletred";
+        else if (path.includes("p3")) color = "#50C878";
+
+
+        bottleBG.style.background = color;
+        setTimeout(() => {
+            belowBG.style.backgroundColor = bottleBG.style.background;
+        }, 1000);
+
+        bottleBG.classList.add("BGanimate");
+        bottleBG.addEventListener("animationend", () => {
+            bottleBG.classList.remove("BGanimate");
+        }, { once: true });
+    });
+})
